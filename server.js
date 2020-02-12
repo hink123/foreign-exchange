b7c1850e52586db01305a3cdd05082afe65e79bf
Will Hinkelman <will.hink24@gmail.com>
2020-02-12 15:50:49 -0800

    backend configured

--- server.js
+++ server.js
@@ -8,5 +8,18 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'facicon.ico')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+// API Routes here
+
+// catch all route
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+});
