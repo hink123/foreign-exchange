29d18e20281115da874a4a95c21482720e99682f
Will Hinkelman <will.hink24@gmail.com>
2020-02-13 14:53:15 -0800

    user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: String
+}, {
+    timestamps: true
+})
