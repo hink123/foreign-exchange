3f8fa8455f7bb696e8780d6af12bb37506955d97
Will Hinkelman <will.hink24@gmail.com>
2020-03-04 15:30:19 -0800

    login controller func

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const SECRET = process.env.SECRET;
 
 module.exports = {
-    signup
+    signup,
+    login
 }
 
 async function signup(req, res) {
@@ -15,6 +16,25 @@ async function signup(req, res) {
         res.json({token});
     } catch (err) {
         res.status(400).json(err);
+    }
+}
+
+async function login(req, res) {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if(!user) {
+            return res.satus(401).json({err: 'bad credentials'});
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(isMatch) {
+                const token = createJWT(user);
+                res.json({token});
+            } else {
+                return res.status(401).json({err: 'bad credentials'});
+            }
+        })
+    } catch (err) {
+        return res.status(401).json(err);
     }
 }
 
