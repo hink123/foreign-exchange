37f159822cbabaeafc24826148e2c80dd36c7307
Will Hinkelman <will.hink24@gmail.com>
2020-03-03 16:37:23 -0800

    login page

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react' 
+
+class LoginPage extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    render() {
+        return (
+            <div>
+                Login Form
+            </div>
+        )
+    }
+}
+
+export default LoginPage;
