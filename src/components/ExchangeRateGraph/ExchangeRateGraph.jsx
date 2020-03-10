8dbbe49dfe2dfe7dc6336d9cd872e568468aca9a
Will Hinkelman <will.hink24@gmail.com>
2020-03-10 15:06:03 -0700

    canvas pages downloaded

--- src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
+++ src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import './ExchangeRateGraph.css';
 
 const ExchangeRateGraph = (props) => {
+
+    const createGraph = () => {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            title:{
+                text: "My First Chart in CanvasJS"              
+            },
+            data: [              
+            {
+                // Change type to "doughnut", "line", "splineArea", etc.
+                type: "column",
+                dataPoints: [
+                    { label: "apple",  y: 10  },
+                    { label: "orange", y: 15  },
+                    { label: "banana", y: 25  },
+                    { label: "mango",  y: 30  },
+                    { label: "grape",  y: 28  }
+                ]
+            }
+            ]
+        });
+        chart.render();
+    }
+
     return (
         <div>
-            Here is the Graph
+            <h1>Here is the graph</h1>
             <button onClick={props.handleNewSearch}>New Search</button>
         </div>
     )
