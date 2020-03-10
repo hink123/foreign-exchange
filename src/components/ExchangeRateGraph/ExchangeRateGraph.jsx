72953f05be66df73509bfd7aa1b12fea2270772e
Will Hinkelman <will.hink24@gmail.com>
2020-03-10 15:44:27 -0700

    change ExchangeRateGraph to class

--- src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
+++ src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
@@ -1,39 +1,49 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import CanvasJSReact from '../../services/canvasjs.react';
 import './ExchangeRateGraph.css';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const ExchangeRateGraph = (props) => {
+const options = {
+    title: {
+      text: `From ${this.props.graphData['Meta Data']['2. From Symbol']} to ${this.props.graphData['Meta Data']['3. To Symbol']}`
+    },
+    data: [{				
+              type: "line",
+              dataPoints: [
+                  { label: "Apple",  y: 10  },
+                  { label: "Orange", y: 15  },
+                  { label: "Banana", y: 25  },
+                  { label: "Mango",  y: 30  },
+                  { label: "Grape",  y: 28  }
+                ]
+        }]
+}
 
-    const options = {
-        title: {
-          text: `From ${props.graphData['Meta Data']['2. From Symbol']} to ${props.graphData['Meta Data']['3. To Symbol']}`
-        },
-        data: [{				
-                  type: "line",
-                  dataPoints: [
-                      { label: "Apple",  y: 10  },
-                      { label: "Orange", y: 15  },
-                      { label: "Banana", y: 25  },
-                      { label: "Mango",  y: 30  },
-                      { label: "Grape",  y: 28  }
-                    ]
-            }]
+class ExchangeRateGraph extends Component {
+
+
+    componentDidMount() {
+        var chart = this.chart;
+		chart.render();
+		
     }
 
-    return (
-        <div>
-            <h1>Here is the graph</h1>
+    render() {
+        return (
             <div>
-                <CanvasJSChart 
-                    options={options}
-                />
+                <h1>Here is the graph</h1>
+                <div>
+                    <CanvasJSChart 
+                        options={options}
+                        onRef={ref => this.chart = ref}
+                    />
+                </div>
+                <button onClick={this.props.handleNewSearch}>New Search</button>
             </div>
-            <button onClick={props.handleNewSearch}>New Search</button>
-        </div>
-    )
+        )
+    }
 }
 
 export default ExchangeRateGraph;
