5ece85b5df0ec6e3ddb13728b94f061acb07fad8
Will Hinkelman <will.hink24@gmail.com>
2020-03-18 17:51:57 -0700

    update interval 60min

--- src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
+++ src/components/ExchangeRateGraph/ExchangeRateGraph.jsx
@@ -31,7 +31,7 @@ class ExchangeRateGraph extends Component {
         for(const property in timeSeriesFX) {
             dataPoints.unshift({
                 // x: new Date(year, month, day + i),
-                x: new Date(property.slice(0, 4), property.slice(5, 7) - 1, property.slice(8)),
+                x: new Date(property.slice(0, 4), property.slice(5, 7) - 1, property.slice(8, 10)),
                 y: parseFloat(timeSeriesFX[property]['1. open'])
             });
             // i-=7;
