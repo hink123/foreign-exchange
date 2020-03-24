41b4034562b80fff3a56a13791355dc80d0acedf
Will Hinkelman <will.hink24@gmail.com>
2020-03-24 15:43:44 -0700

    nav format

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,16 +14,25 @@ const NavBar = (props) => {
             </button>
         </div>
         <div id="navbarDrop" className="navbar-menu">
-            <Link to='/favorites' className="navbar-item navbar-right">Favorites</Link>
-            <Link to='' className="navbar-item navbar-right" onClick={props.handleLogout}>Logout</Link>
-            <div className="navbar-item navbar-right">{props.user.name}</div>
+            <Link to='/favorites' className="navbar-item">Favorites</Link>
+            <Link to='' className="navbar-item" onClick={props.handleLogout}>Logout</Link>
+            <div className="navbar-item navbar-end">{props.user.name}</div>
         </div>
     </div>
     :
     <div className="navbar is-dark">
-        <Link to='/' className="navbar-item navbar-start">Home</Link>
-        <Link to='/login' className="navbar-item">Login</Link>
-        <Link to='/signup' className="navbar-item">Signup</Link>
+        <div className='navbar-brand'>
+            <Link to='/' className="navbar-item">Home</Link>
+            <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+            </button>
+        </div>
+        <div id='navbarDrop' className="navbar-menu">
+            <Link to='/login' className="navbar-item">Login</Link>
+            <Link to='/signup' className="navbar-item">Signup</Link>
+        </div>
     </div>
     return (
         <div>
