fba81e15231542df61f9311f47d97be8c69d7360
Will Hinkelman <will.hink24@gmail.com>
2020-03-23 17:47:34 -0700

    nav color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,14 +4,14 @@ import './NavBar.css';
 
 const NavBar = (props) => {
     let nav = props.user ?
-    <div className='navbar'>
+    <div className='navbar is-dark'>
         <Link to='/' className="navbar-item navbar-start">Home</Link>
         <Link to='/favorites' className="navbar-item">Favorites</Link>
         <Link to='' className="navbar-item" onClick={props.handleLogout}>Logout</Link>
         <div className="navbar-end">{props.user.name}</div>
     </div>
     :
-    <div className="navbar">
+    <div className="navbar is-dark">
         <Link to='/' className="navbar-item navbar-start">Home</Link>
         <Link to='/login' className="navbar-item">Login</Link>
         <Link to='/signup' className="navbar-item">Signup</Link>
