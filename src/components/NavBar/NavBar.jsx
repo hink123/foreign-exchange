91314113fa09f7e85ec7d2d3acc7e2d9a76ac0eb
Will Hinkelman <will.hink24@gmail.com>
2020-03-23 18:02:10 -0700

    nav burger

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,10 +5,19 @@ import './NavBar.css';
 const NavBar = (props) => {
     let nav = props.user ?
     <div className='navbar is-dark'>
-        <Link to='/' className="navbar-item navbar-start">Home</Link>
-        <Link to='/favorites' className="navbar-item">Favorites</Link>
-        <Link to='' className="navbar-item" onClick={props.handleLogout}>Logout</Link>
-        <div className="navbar-end">{props.user.name}</div>
+        <div className='navbar-brand'>
+            <Link to='/' className="navbar-item">Home</Link>
+            <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+            </button>
+        </div>
+        <div id="navbarDrop" className="navbar-menu">
+            <Link to='/favorites' className="navbar-item navbar-right">Favorites</Link>
+            <Link to='' className="navbar-item navbar-right" onClick={props.handleLogout}>Logout</Link>
+            <div className="navbar-item navbar-right">{props.user.name}</div>
+        </div>
     </div>
     :
     <div className="navbar is-dark">
