461c7b63ae0613802ef0a421567f534804a1e015
Will Hinkelman <will.hink24@gmail.com>
2020-03-26 11:39:48 -0700

    navBar switched to a class

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,52 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = (props) => {
-    let nav = props.user ?
-    <div className='navbar is-dark'>
-        <div className='navbar-brand'>
-            <Link to='/' className="navbar-item navbar-text">Foreign Exchange</Link>
-            <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-            </button>
-        </div>
-        <div id="navbarDrop" className="navbar-menu">
-            <div className="navbar-item">
-                <Link to='/favorites' className='navbar-text'>Favorites</Link>
+class NavBar extends Component {
+
+    state={
+        dropDown: false
+    }
+
+    toggleDropDown = () => {
+        this.setState({
+            dropDown: !this.state.dropDown
+        })
+    }
+    render() {
+        let nav = this.props.user ?
+        <div className='navbar is-dark'>
+            <div className='navbar-brand'>
+                <Link to='/' className="navbar-item navbar-text">Foreign Exchange</Link>
+                <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop" onClick={this.toggleDropDown}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                </button>
             </div>
-            <div className='navbar-item navbar-end'>
-                <Link to='' className=" navbar-text" onClick={props.handleLogout}>Logout</Link>
-            </div>
-            <div className="navbar-item navbar-name">{props.user.name}</div>
-        </div>
-    </div>
-    :
-    <div className="navbar is-dark">
-        <div className='navbar-brand'>
-            <Link to='/' className="navbar-item navbar-text">Foreign Exchange</Link>
-            <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-            </button>
-        </div>
-        <div id='navbarDrop' className="navbar-menu">
-            <div className='navbar-item navbar-end'>
-                <Link to='/login' className="navbar-text">Login</Link>
-            </div>
-            <div className='navbar-item'>
-                <Link to='/signup' className="navbar-text">Signup</Link>
+            <div id="navbarDrop" className="navbar-menu">
+                <div className="navbar-item">
+                    <Link to='/favorites' className='navbar-text'>Favorites</Link>
+                </div>
+                <div className='navbar-item navbar-end'>
+                    <Link to='' className="navbar-text" onClick={this.props.handleLogout}>Logout</Link>
+                </div>
+                <div className="navbar-item navbar-name">{this.props.user.name}</div>
             </div>
         </div>
-    </div>
-    return (
-        <div>
-            {nav}
+        :
+        <div className="navbar is-dark">
+            <div className='navbar-brand'>
+                <Link to='/' className="navbar-item navbar-text">Foreign Exchange</Link>
+                <button className="navbar-burger burger is-active" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                </button>
+            </div>
+            <div id='navbarDrop' className="navbar-menu is-active">
+                <div className='navbar-item navbar-end'>
+                    <Link to='/login' className="navbar-text">Login</Link>
+                </div>
+                <div className='navbar-item'>
+                    <Link to='/signup' className="navbar-text">Signup</Link>
+                </div>
+            </div>
+        
         </div>
-    )
+        return (
+            <div>
+                {nav}
+            </div>
+        )
+    }
 }
 
 export default NavBar;
