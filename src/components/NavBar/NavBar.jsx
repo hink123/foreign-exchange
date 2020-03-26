d646272f37890271cf4e5f4a387584fd06842a9c
Will Hinkelman <will.hink24@gmail.com>
2020-03-26 10:55:19 -0700

    navbar hover

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = (props) => {
     let nav = props.user ?
     <div className='navbar is-dark'>
         <div className='navbar-brand'>
-            <Link to='/' className="navbar-item">Home</Link>
+            <Link to='/' className="navbar-item navbar-text">Home</Link>
             <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarDrop">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -14,10 +14,12 @@ const NavBar = (props) => {
             </button>
         </div>
         <div id="navbarDrop" className="navbar-menu">
-            <div className="navbar-item test-nav">
+            <div className="navbar-item">
                 <Link to='/favorites' className='navbar-text'>Favorites</Link>
             </div>
-            <Link to='' className="navbar-item navbar-text" onClick={props.handleLogout}>Logout</Link>
+            <div className='navbar-item'>
+                <Link to='' className=" navbar-text" onClick={props.handleLogout}>Logout</Link>
+            </div>
             <div className="navbar-item navbar-end navbar-text">{props.user.name}</div>
         </div>
     </div>
