6d2fab142df034ecb5420aed4b2387ccc67bed81
Will Hinkelman <will.hink24@gmail.com>
2020-04-16 15:08:19 -0700

    router favorite

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,5 +4,6 @@ const usersCtrl = require('../../controllers/users');
 
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
+router.post('/favorite', usersCtrl.addFavorite);
 
 module.exports = router;
